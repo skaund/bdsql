3d42b9b0d7659178a7d7db1a7501fa67a4f98dd0
skaund <63222456+skaund@users.noreply.github.com>
2021-03-21 21:54:56 +0400

    Добавьте файлы проекта.

---
+++ AdoSqlite/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdoSqlite
+{
+    public partial class Form1 : Form
+    {
+        private List<DAL.User> _list;
+
+        public Form1()
+        {
+            InitializeComponent();
+   
+
+            _list = new List<DAL.User>();
+            bsUser.DataSource = _list;
+            dataGridView1.AutoGenerateColumns = true;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _list.Clear();
+
+            List<DAL.User> list = (List<DAL.User>)DAL.SqliteHelper.GetUsers();
+            if (list != null && list.Count > 0)
+            {
+              _list.AddRange(list);
+               bsUser.ResetBindings(false);
+            }    
+        }
+    }
+}
